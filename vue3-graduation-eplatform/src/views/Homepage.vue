7067b9bae5d543cac190da2c30256ba6f2d81380
KumaPan <cahelligar0707@gmail.com>
2023-06-29 14:34:18 +0200

    front end changes
    - better onboarding progress

--- vue3-graduation-eplatform/src/views/Homepage.vue
+++ vue3-graduation-eplatform/src/views/Homepage.vue
@@ -1,221 +1,58 @@
 <template>
-    <div>
-    <div class="container">
-        <div class="row justify-content-md-center mt-5 pb-5">
-                <h1 id="subject">Omnichannel E-Learning platform</h1>
-            <div class="col-md-6">
-                <img src="https://picsum.photos/id/8/600/400" alt="">
-            </div>
-            <!-- <div class="col-md-7">
-                <p class="mt-4">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                    It has survived not only five centuries, but also the leap into electronic typesetting, 
-                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-                    Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-            </div> -->
+    <div class="container-fluid">
+    
+        <div class="row justify-content-md-center pb-5" id="bgColor1" style="height: 25rem;">
+              <div class="content-container">
+                    <div class="col-auto mt-5 pe-5 justify-content-center">
+                    <h2 class="text-white subheadingText">A great solution for your business!</h2>
+                    <p class="insectionText"><b>Learning made easy.</b> Why take the long when there's a simple short cut right here, right now!</p>
+                </div>
+                <div class="col-auto mt-5 pb-5 ms-5">
+                    <!-- <img src="https://picsum.photos/id/8/600/400" alt=""> -->
+                </div>
+              </div>  
         </div> 
-    </div>
 
-    <div class="container-fluid" id="second-section">
-        <div class="row justify-content-md-center pt-2 pb-3">
-            <div class="col-md-3">
-            <h2 id="subject">Trending</h2>
-                <div id="carouselExampleCaptions" class="carousel slide pb-5">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="https://picsum.photos/id/4/300/200" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block text-black">
-                                    <h5>Rouge Ai Chat bots</h5>
-                                    <p>Many users of AI be aware of upcomming problems</p>
-                                </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="https://picsum.photos/id/20/300/200" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block text-black">
-                                    <h5>Second slide label</h5>
-                                    <p>Some representative placeholder content for the second slide.</p>
-                                </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="https://picsum.photos/id/24/300/200" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block text-black">
-                                    <h5>Third slide label</h5>
-                                    <p>Some representative placeholder content for the third slide.</p>
-                                </div>
-                        </div>
-                    </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
+        <div class="row">
+            <div class="col-md-12" id="bgBland" style="height: 7rem;">
+                <h2 class="text-center headingText" >Supporting Businesess</h2>
+                <div class="d-flex justify-content-center">
+                    <!-- <img src="@/assets/images/123tcslogo.png" alt="tcs" style="height: 100%; width: 100%;"> -->
+                    <!-- <img src="@/assets/images/ccvlogo.png" alt="ccv" style="height: 100%; width: 100%;"> -->
+                    <!-- <img src="@/assets/images/ccvshoplogo.png" alt="ccv-shop" style="height: 100%; width: 100%;"> -->
+                    <!-- <img src="@/assets/images/rabobanklogo.png" alt="rabo" style="height: 100%; width: 100%;"> -->
                 </div>
             </div>
-            <div class="col-md-6 pt-5">
-                <h3>The realm of uncontrolable AI chatbots</h3>
-                    <h6>May 5th 2023 | Lumera tech</h6>
-                    <p class="text-white">
-                        The development of AI technologies has led to a range of applications across industries, 
-                        including healthcare, finance, transportation, and more. Additionally, there has been a 
-                        growing interest in ethical considerations surrounding AI, such as transparency, accountability, 
-                        and bias. Other notable technology trends include the proliferation of Internet of Things (IoT) 
-                        devices, 5G networks, and the continued growth of cloud computing. These trends have the potential 
-                        to transform the way we live and work, and are expected to have a significant impact on society in the 
-                        coming years.
-                        <button type="button" class="btn btn-sm">Read more</button>
-                    </p>
-                    
+        </div>
+    
+
+        
+            <div class="row justify-content-md-center" id="bgColor2">
+                <h2 class="text-center headingText">About</h2>
+                <div class="col-md-6" >
+                <h4 class="subheadingText">The Application</h4>
+                <p class="insectionText text-start">The<b> E-Marketing app</b> was created to aid businesses in successful creation of products
+                and the development of their brand. By looking at smaller parts using the four p's of marketing matrix
+                and use of gamification methods, this tools will supply that motivaton needed to go on.
+                </p>
+                <p class="insectionText">Here is a list of features to expect</p>
+                <p class="insectionText"><i class="bi bi-clipboard-check-fill"></i> Planning</p>
+                <p class="insectionText"><i class="bi bi-keyboard-fill"></i> Pracice</p>
+                <p class="insectionText"><i class="bi bi-info-circle-fill"></i> Informational content</p>
+                <p class="insectionText"><i class="bi bi-joystick"></i> Gamification elements and rewards</p>
+                <p class="insectionText"><i class="bi bi-people-fill"></i> Team setup</p>
+                </div>
+
+                <div class="col-md-6" >
+                <h4 class="subheadingText">The company behind it</h4>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="container-fluid" id="third-section">
-        <div class="pt-5">
-            <div class="">
-                <!-- <img src="https://picsum.photos/id/180/1000/500" alt=""> -->
-            </div>
-        </div>
-    </div>
-    <div class="">
-  <!-- Footer -->
-  <footer
-          class="text-center text-lg-start text-dark"
-          style="background-color: #ECEFF1"
-          >
-    <!-- Section: Social media -->
-    <section
-             class="d-flex justify-content-between p-4 text-white"
-             style="background-color: #06AD99"
-             >
-      <!-- Left -->
-      <div class="me-5">
-        <span>Get connected with us on social networks:</span>
-      </div>
-      <!-- Left -->
+            
+    
 
-      <!-- Right -->
-      <div>
-        <a href="" class="text-white me-4">
-            <i class="bi bi-twitter"></i>
-        </a>
-        <a href="" class="text-white me-4">
-            <i class="bi bi-facebook"></i>
-        </a>
-        <a href="" class="text-white me-4">
-            <i class="bi bi-google"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="bi bi-instagram"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="bi bi-linkedin"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="bi bi-github"></i>
-        </a>
-      </div>
-      <!-- Right -->
-    </section>
-    <!-- Section: Social media -->
+    
+        
 
-    <!-- Section: Links  -->
-    <section class="">
-      <div class="container text-center text-md-start mt-5">
-        <!-- Grid row -->
-        <div class="row mt-3">
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <!-- Content -->
-            <h6 class="text-uppercase fw-bold">Actablue</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p>
-              -Get text for company here later after meeting-
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Products</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Useful links</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <!-- <p>
-              <a href="#!" class="text-dark">Your Account</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">Become an Affiliate</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">Shipping Rates</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">Help</a>
-            </p> -->
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Contact</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p><i class="fas fa-home mr-3"></i>Gegevens Ceintuurbaan Noord 126, 9301 NZ Roden</p>
-            <!-- <p><i class="fas fa-envelope mr-3"></i></p> -->
-            <p><i class="fas fa-phone mr-3"></i>088 110 1000</p>
-            <!-- <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p> -->
-          </div>
-          <!-- Grid column -->
-        </div>
-        <!-- Grid row -->
-      </div>
-    </section>
-    <!-- Section: Links  -->
-
-    <!-- Copyright -->
-    <div
-         class="text-center p-3"
-         style="background-color: rgba(0, 0, 0, 0.2)"
-         >
-      © 2023 Copyright:
-      <a class="text-dark" href="https://actablue.com"
-         >Actablue</a
-        >
-    </div>
-    <!-- Copyright -->
-  </footer>
-  <!-- Footer -->
-</div>
     </div>
 </template>
 
@@ -231,24 +68,41 @@ export default{
 </script>
 
 <style scoped>
-   body{
-        background-color: #048171 !important;
+   #bgColor1{
+        background-color: #048171;
+    }
+   #bgColor2{
+    background-color: #06AD99;
+    }
+    #bgBland{
+        background-color: rgb(236, 236, 236);
     }
 
     #subject{
         text-align: center;
     }
-
-    #second-section{
-        background-color: #06AD99;
-    }
-
-    p{
-        /* color: white; */
-    }
     
-    #third-section{
-        background-image: url(https://picsum.photos/id/180/1000/500);
-        height: 500px;
+   
+    /* .leftTxt{
+       text-align: left;
+       position: absolute;
+       left: auto;
+    } */
+
+    .headingText{
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        color: #152238;
+        /* font-weight: bold; */
     }
+
+    .subheadingText{
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        color: #152238;
+    }
+
+.insectionText{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    color: white;
+    font-size: medium;
+}
 </style>
